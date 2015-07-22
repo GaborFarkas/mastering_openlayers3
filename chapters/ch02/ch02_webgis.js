bdb44395e697be9fd95d269cda3375d179c053b7
GaborFarkas <raandal73@gmail.com>
2015-07-22 18:46:36 +0200

    Finalize Ch02

--- chapters/ch02/ch02_webgis.js
+++ chapters/ch02/ch02_webgis.js
@@ -1,25 +1,24 @@
 function init() {
     document.removeEventListener('DOMContentLoaded', init);
-    var vectorLayer = new ol.layer.Vector({
-        source: new ol.source.Vector({
-            format: new ol.format.GeoJSON({
-                defaultDataProjection: 'EPSG:4326'
-            }),
-            url: '../../res/world_capitals.geojson',
-            attributions: [
-                new ol.Attribution({
-                    html: 'World Capitals © Natural Earth'
-                })
-            ]
-        })
-    })
     var map = new ol.Map({
         target: 'map',
         layers: [
             new ol.layer.Tile({
                 source: new ol.source.OSM()
             }),
-            vectorLayer
+            new ol.layer.Vector({
+                source: new ol.source.Vector({
+                    format: new ol.format.GeoJSON({
+                        defaultDataProjection: 'EPSG:4326'
+                    }),
+                    url: '../../res/world_capitals.geojson',
+                    attributions: [
+                        new ol.Attribution({
+                            html: 'World Capitals © Natural Earth'
+                        })
+                    ]
+                })
+            })
         ],
         controls: [
             //Define the default controls
